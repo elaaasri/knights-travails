04580275c90c7c1ce233ab2c1defc88a2209eb2b
elaaasri <anas_elasri@outlook.fr>
2024-06-05 16:07:31 +0100

    clean code, adding findShortestPath(start, end), returns the shortest path from start move to end move

--- script.js
+++ script.js
@@ -1,75 +1,102 @@
-// class board :
 class Board {
   constructor() {
     this.rows = 8;
     this.columns = 8;
     this.board = [];
   }
-  // returns a 2D board :
+  // create a 2D array representing the board :
   buildGraph() {
-    // creating 2D array :
     for (let i = 0; i < this.rows; i++) {
       this.board[i] = [];
       for (let j = 0; j < this.columns; j++) {
-        this.board[i][j] = 0;
+        this.board[i][j] = 0; // initialize each node as 0.
       }
     }
   }
-  knightMoves(start, end) {
-    // build 2D array :
-    this.buildGraph();
-    // start knight move :
-    const [startRow, startColumn] = start;
-    // end knight move :
-    const [endRow, endColumn] = end;
-    // all other knight moves :
-    const otherKnightMoves = [
-      [startRow - 1, startColumn - 2],
-      [startRow + 1, startColumn - 2],
-      [startRow - 1, startColumn + 2],
-      [startRow + 1, startColumn + 2],
-      [startRow - 2, startColumn - 1],
-      [startRow - 2, startColumn + 1],
-      [startRow + 2, startColumn - 1],
-      [startRow + 2, startColumn + 1],
-    ];
-    // adding legal knight start/end move :
-    let knightStartMove = this.isWithinBounds(startRow, startColumn);
-    let knightEndMove = this.isWithinBounds(endRow, endColumn);
-    // adding moves to board :
-    if (knightStartMove && knightEndMove) {
-      this.board[startRow][startColumn] = "s"; // start move.
-      this.board[endRow][endColumn] = "e"; // end move.
-
-      // other legal knight moves :
-      const filterdOtherKnightMoves = otherKnightMoves.filter((move) =>
-        this.isWithinBounds(move[0], move[1])
-      );
-      filterdOtherKnightMoves.map((move) => {
-        this.board[move[0]][move[1]] = "m";
-        return;
-      });
-    } else return "start and end moves must be legal in the board!";
-    console.log("#".repeat(30));
-    this.board.map((a) => console.log(a));
-  }
-  // getShortestPath(newMoves, endMove) {
-  //   newMoves.forEach((move) => {
-  //     let queue = [move];
-  //     currentMove = queue.pop();
-  //     console.log(move);
-  //     this.knightMoves(move, endMove);
-  //   });
-  //   return `=> You made it in ${
-  //     allPaths.length
-  //   } moves!  Here's your path: \n ${JSON.stringify(allPaths)}`;
-  // }
-  // checks if the given move indices is in the 2D array board :
+  // Check if a given move is within the bounds of the board
   isWithinBounds(x, y) {
     return x >= 0 && x < this.rows && y >= 0 && y < this.columns;
   }
+  // find the shortest path using BFS :
+  findShortestPath(start, end) {
+    const queue = [start]; // queue to implement the fun on each other move.
+    const path = []; // stores path.
+    const parentMap = new Map(); // stores each node's predecessor to reconstruct the path later.
+    parentMap.set(start.toString(), null); // setting the parent of each move.
+
+    // iterates over all other legal knight moves :
+    while (queue.length > 0) {
+      let currentMove = queue.shift();
+      const [currentRow, currentColumn] = currentMove;
+      const [endRow, endColumn] = end;
+
+      // // shows the board on each iteration :
+      // console.log("#".repeat(30));
+      // this.board.map((a) => console.log(a));
+
+      // if we reach the end, construct the path :
+      if (currentRow === endRow && currentColumn === endColumn) {
+        // constructuring the path :
+        while (currentMove) {
+          path.push(currentMove); // stores current.
+          currentMove = parentMap.get(currentMove.toString()); // updates currentMove to it's predecessor.
+        }
+        return `=> You made it in ${
+          path.length
+        } moves! Here's your path: ${JSON.stringify(path.reverse())}`;
+      }
+
+      // gets all other possible knight moves :
+      const allKnightMoves = [
+        [currentRow - 1, currentColumn - 2],
+        [currentRow + 1, currentColumn - 2],
+        [currentRow - 1, currentColumn + 2],
+        [currentRow + 1, currentColumn + 2],
+        [currentRow - 2, currentColumn - 1],
+        [currentRow - 2, currentColumn + 1],
+        [currentRow + 2, currentColumn - 1],
+        [currentRow + 2, currentColumn + 1],
+      ];
+      // filters legal knight moves :
+      const legalKnightMoves = allKnightMoves.filter((move) =>
+        this.isWithinBounds(move[0], move[1])
+      );
+
+      // explores all other legal knight moves :
+      for (const move of legalKnightMoves) {
+        const [moveRow, moveColumn] = move;
+        this.board[moveRow][moveColumn] = "m"; // adding "m" as the other moves.
+
+        // helps to not revisit moves we already encountered :
+        if (!parentMap.has(move.toString())) {
+          parentMap.set(move.toString(), currentMove);
+          queue.push(move);
+        }
+      }
+    }
+    return "no possible path found.";
+  }
+  //
+  knightMoves(start, end) {
+    // builds board :
+    this.buildGraph();
+    const [startRow, startColumn] = start;
+    const [endRow, endColumn] = end;
+    // ensures that start and end moves is legal :
+    const startMove = this.isWithinBounds(startRow, startColumn);
+    const endMove = this.isWithinBounds(endRow, endColumn);
+    // adding start and end moves to the board if there are both legal :
+    if (startMove && endMove) {
+      this.board[startRow][startColumn] = "s"; // adding "s" as the start.
+      this.board[endRow][endColumn] = "e"; // adding "e" as the end.
+      return this.findShortestPath(start, end); // finds the shortest path.
+    } else {
+      return "Start and end moves must be legal within the board!";
+    }
+  }
 }
+// Test the board and knightMoves function
 const board = new Board();
-const start = [1, 1];
-const end = [6, 6];
+const start = [3, 3];
+const end = [5, 3];
 console.log(board.knightMoves(start, end));
